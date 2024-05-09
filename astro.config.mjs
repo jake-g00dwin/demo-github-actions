f2f92dc8d83a6684291a3dbf7214292db3042a92
jakeg00dwin <jakegoodwin@gorge.works>
2024-05-09 13:14:03 -0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import prefetch from "@astrojs/prefetch";
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
   site: "https://adulbrich.github.io",
+  site: "https://jake-g00dwin.github.io",
   base: "/demo-github-actions",
 });
